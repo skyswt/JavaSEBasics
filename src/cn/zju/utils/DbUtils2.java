c8655fbc40b360eb0a7905381fbfb4166f8cd613
swt <2515767544@qq.com>
2020-08-18 23:58:19 +0800

    Druid连接池

---
+++ src/cn/zju/utils/DbUtils2.java
@@ -0,0 +1,39 @@
+package cn.zju.utils;
+
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DbUtils2 {
+    private static DruidDataSource ds;
+
+    static {
+        Properties properties = new Properties();
+        try {
+            properties.load(DbUtils2.class.getResourceAsStream("database.properteis"));
+            ds = (DruidDataSource) DruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //获取连接对象
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    //释放资源
+
+}
+
